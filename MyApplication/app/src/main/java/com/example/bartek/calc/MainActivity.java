ee14df8c4c0f4c778f232cf7388e85d914aa1f22
bartek <bartek.k.ziolkowski@gmail.com>
2017-04-29 17:55:09 +0200

    "dodawanie"

--- MyApplication/app/src/main/java/com/example/bartek/calc/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/bartek/calc/MainActivity.java
@@ -8,11 +8,14 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.Stack;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private TextView textview;
     private String display;
+    private Stack stack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +23,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textview = (TextView) findViewById(R.id.editText);
         display = new String();
+        stack = new Stack();
 
     }
 
     public void onClick(View view) {
         button = (Button) view;
-        display += button.getText().toString();
+        if(display.equals("0")) {
+            display = button.getText().toString();
+        } else if(button.getText().toString().equals("+")) {
+                stack.push(display);
+                stack.push("+");
+             display += button.getText().toString();
+        } else {
+            display += button.getText().toString();
+        }
+
         textview.setText(display);
 
     }
@@ -34,6 +47,26 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) view;
         display = "";
         textview.setText(display);
+        stack.removeAllElements();
+
+    }
+
+    public void onResult(View view) {
+        int tmpX = 0;
+        int tmpY = 0;
+        String operation = "";
+        if(!stack.empty()) {
+            tmpX = Integer.parseInt((String) stack.pop());
+            operation = (String) stack.pop();
+            tmpY = Integer.parseInt((String) stack.pop());
+            switch(operation) {
+                case "+": textview.setText( Integer.toString(tmpX+tmpY));
+            }
+        }
+
+
+
+
 
     }
 }
